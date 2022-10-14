a540d641ec197378f8b3fce7aea337a0bdf0a369
Tony Wu <bobacrypto2@gmail.com>
2022-10-14 02:51:20 -0700

    Create better property to calculate age of a new user based off of a birthday value to have both age + birthday

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-let user = {
-  get name() {
-    return this._name;
-  },
+function User(name, birthday) {
+  this.name = name,
+  this.birthday = birthday,
 
-  set name(value) {
-    if (value.length < 4) {
-      alert("Name is too short, need at least 4 characters");
-      return;
+  Object.defineProperty(this, "age", {
+    get() {
+      let currentYear = new Date().getFullYear();
+      return currentYear - this.birthday.getFullYear();
     }
-    this._name = value;
-  }
-};
+  })
+}
 
-user.name = "Tony";
-alert(user.name) // Tony | The input meets the requirements!
-
-user.name = "Kat";
-// alert(user.name) will meet our if condition and alert the user that the name is too short.
+let tony = new User("Tony", new Date(1998, 1, 26));

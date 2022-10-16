3ec493944fa430c8400c0ec00bc1a0d6e7a493fd
Tony Wu <bobacrypto2@gmail.com>
2022-10-16 09:35:35 -0700

    Javascript extend class | Extend a non-constructible object

--- script.js
+++ script.js
@@ -1,41 +1,18 @@
-// Practice with converting a class written in functional style in ' class ' syntax
-// via https://javascript.info/class
-class Clock {
-
-  constructor({template}) {
-    this.template = template;
+const Animal = {
+  speak() {
+    console.log(`${this.name} makes a noise`);
   }
+};
 
-  render() {
-    let date = new Date();
-
-    let hours = date.getHours();
-    if (hours < 10) hours = '0' + hours;
-
-    let mins = date.getMinutes();
-    if (mins < 10) mins = '0' + mins;
-
-    let secs = date.getSeconds();
-    if (secs < 10) secs = '0' + secs;
-
-    let output = this.template
-      .replace('h', hours)
-      .replace('m', mins)
-      .replace('s', secs);
-
-    console.log(output);
-  }
-
-  stop() {
-    clearInterval(this.timer);
-  }
-
-  start() {
-    this.render();
-    // Pass a wrapper function to fix "losing this"
-    this.timer = setInterval(() => this.render(), 1000);
+class Dog {
+  constructor(name) {
+    this.name = name;
   }
 }
 
-let clock = new Clock({template: 'h:m:s'});
-clock.start(); // Live ticking clock within console
+// Extending class to Animal object
+Object.setPrototypeOf(Dog.prototype, Animal);
+
+const a = new Dog('April');
+
+// a.speak();

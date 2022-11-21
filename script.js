da612805fef7893610da7d81c868db6368a8a340
Tony Wu <bobacrypto2@gmail.com>
2022-11-21 00:37:15 -0800

    Redoing palindrome test

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-const barker = (state) => ({
-  bark: () => console.log('Woof, I am ' + state.name)
-})
+let string = "racecar!";
 
-const driver = (state) => ({
-  drive: () => state.position = state.speed + state.position
-})
+const palindroms = function(str) {
+    let chars = str.split('');
+  
+    let reverse = chars.reduce( 
+        (acc, item) => { return (item == '!' || item == ',' || item == '.' || item == ' ') ? acc : item + acc; }
+    )
 
-const killer = (state) => ({
-  kill: () => console.log(`I ${state.name} will murder you!`)
-})
+    let original = chars.reduce(
+        (acc, item) => { return (item == '!' || item == ',' || item == '.' || item == ' ') ? acc : acc + item }
+    )
 
-const murderRobotDog = (name) => {
-  let state = {
-    name,
-    speed: 100,
-    position: 0,
-  }
-  return Object.assign (
-    {},
-    barker(state),
-    driver(state),
-    killer(state),
-  )
+    console.log(reverse);
+    console.log(original);
 }
-
-const april = murderRobotDog('April');
-const kelly = murderRobotDog('Kelly');

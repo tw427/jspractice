7afb04f98cf5b6cc1097f760eacf373edc5d32e7
Tony W <bobacrypto2@gmail.com>
2023-01-19 08:56:50 -0800

    Implemented my first API!

--- script.js
+++ script.js
@@ -1,64 +1,9 @@
-//add html elements
-const gridContainer = document.getElementById("gridContainer");
-const newGridButton = document.getElementById("newGridButton");
+const img = document.querySelector('img')
 
-//funtion to create grid
-function createGrid(numSquares){
-    //create grid
-    for (let rows = 0; rows < numSquares * numSquares; rows++) {
-        
-        //create cell element    
-        let cell = document.createElement("div");
-        
-        //name cell element       
-        cell.setAttribute("id", "cell"); 
-        
-        cell.addEventListener('mouseover', changeColor)
-        
-        //append to DOM
-        gridContainer.appendChild(cell)
-    }
-
-    gridContainer.setAttribute('style', `grid-template-columns: repeat(${numSquares}, 1fr)`);
-}
-
-
-
-//function to change color 
-function changeColor(e){
-    e.target.style.backgroundColor = 'black';
-}
-
-//button function for prompt 
-newGridButton.addEventListener('click', function(){
-    let numSquares = prompt("Please enter the number of squares you'd like for each side of your grid");
-    removeGrid();
-    createGrid(numSquares);
-});
-
-//function to remove old grid 
-function removeGrid(){
-    gridContainer.replaceChildren();
-}
-
-//adjust size of squares based on input
-// function resizeGrid(numSquares){
-//     let cell = document.getElementById("cell")
-
-//     if (1 < numSquares >25){
-//         cell.style.height = "75px";
-//         cell.style.width = "75px";}
-//     else if (26 < numSquares > 50){
-//         cell.style.height = "50px";
-//         cell.style.width = "50px";
-//     }
-//     else if (51 < numSquares > 75 ){
-//         cell.style.height = "50px";
-//         cell.style.width = "50px";
-//     }
-//     else if (76 < numSquares > 99){
-//         cell.style.height = "50px";
-//         cell.style.width = "50px";
-//     }
-//     gridContainer.style.gridTemplate = `repeat(${numSquares}, 1fr)/ repeat(${numSquares}, 1 fr)`;
-//     }
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=9Ax4N9eoukEOtBNJ4J3ONOyaTfzvA7aX&s=cats', {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        img.src = response.data.images.original.url;
+    });

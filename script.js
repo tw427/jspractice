ed4936e3638ef9d6027719cdaeae57e07ca463fe
Tony W <bobacrypto2@gmail.com>
2023-01-19 10:32:24 -0800

    Add search for specific gifs and a button to cycle a new gif

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const img = document.querySelector('img')
+const btn = document.getElementById('img-btn')
+const search = document.querySelector('input[type="text"]');
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=9Ax4N9eoukEOtBNJ4J3ONOyaTfzvA7aX&s=cats', {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        img.src = response.data.images.original.url;
-    });
+btn.addEventListener('click', () => {
+    if (search.value == '') {
+        return;
+    } else {
+        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=9Ax4N9eoukEOtBNJ4J3ONOyaTfzvA7aX&s=${search.value}`, {mode: 'cors'})
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(response) {
+                img.src = response.data.images.original.url;
+            })
+            .catch(function(error) {
+                console.log(error);
+                alert('Was not able to find a gif for that specific keyword');
+            });
+    }
+});

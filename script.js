20756aaadd7234c6eca40290306cc701eccf74b0
Tony W <bobacrypto2@gmail.com>
2023-01-21 01:13:24 -0800

    Refactor code with async / await syntax and also try / catch

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ btn.addEventListener('click', () => {
     if (search.value == '') {
         return;
     } else {
-        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=9Ax4N9eoukEOtBNJ4J3ONOyaTfzvA7aX&s=${search.value}`, {mode: 'cors'})
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(response) {
-                img.src = response.data.images.original.url;
-            })
-            .catch(function(error) {
-                console.log(error);
-                alert('Was not able to find a gif for that specific keyword');
-            });
+        async function getCats() {
+        try {
+            const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=9Ax4N9eoukEOtBNJ4J3ONOyaTfzvA7aX&s=${search.value}`, {mode: 'cors'})
+            const catData = await response.json();
+            img.src = catData.data.images.original.url;
+        } catch(error) {
+            console.log(error);
+            alert('Was not able to find a gif for that specific keyword');
+        };
+        }
+    getCats();
     }
 });

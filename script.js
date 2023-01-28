c948d680ccafa5326af6c485f7739fe2331a8466
Tony W <bobacrypto2@gmail.com>
2023-01-28 12:42:07 -0800

    Sum all numbers with recursion

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
-// const img = document.querySelector('img')
-// const btn = document.getElementById('img-btn')
-// const search = document.querySelector('input[type="text"]');
+// For loop solution
 
-// btn.addEventListener('click', () => {
-//     if (search.value == '') {
-//         return;
-//     } else {
-//         async function getCats() {
-//         try {
-//             const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=9Ax4N9eoukEOtBNJ4J3ONOyaTfzvA7aX&s=${search.value}`, {mode: 'cors'})
-//             const catData = await response.json();
-//             img.src = catData.data.images.original.url;
-//         } catch(error) {
-//             console.log(error);
-//             alert('Was not able to find a gif for that specific keyword');
-//         };
-//         }
-//     getCats();
+// function sumTo(n) {
+//     let sum = 0;
+//     let num = n;
+//     for (let i = 0; i < n; i++) {
+//         sum += num;
+//         num--;
 //     }
-// });
 
-function doubleAfter2Seconds(x) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(x * 2);
-        }, 2000);
-    });
-}
+//     return sum;
+// }
 
-async function addAsync(x) {
-    const a = await doubleAfter2Seconds(10);
-    const b = await doubleAfter2Seconds(20);
-    const c = await doubleAfter2Seconds(30);
-    return x + a + b + c;
-}
+// Recursive solution
+
+// function sumTo(n) {
+//     if (n == 1) {
+//         return n;
+//     } else {
+//         return n + sumTo(n - 1);
+//     }
+// }
+
+// Arithmetic progression formula solution
+
+// function sumTo(n) {
+//     return n * (n + 1) / 2;
+// }

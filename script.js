64d1c370b628e97f00d00d8d4c78cd2c9e6d05b6
Tony W <bobacrypto2@gmail.com>
2023-01-28 21:29:08 -0800

    recursion with factorial numbers

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// For loop solution
-
-// function sumTo(n) {
-//     let sum = 0;
-//     let num = n;
-//     for (let i = 0; i < n; i++) {
-//         sum += num;
-//         num--;
-//     }
-
-//     return sum;
-// }
-
-// Recursive solution
-
-// function sumTo(n) {
-//     if (n == 1) {
-//         return n;
-//     } else {
-//         return n + sumTo(n - 1);
-//     }
-// }
-
-// Arithmetic progression formula solution
-
-// function sumTo(n) {
-//     return n * (n + 1) / 2;
-// }
+function factorial(n) {
+    return n == 1 ? n : n * factorial(n - 1);
+}

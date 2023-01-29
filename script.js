f7d317a42ce1a4891995f3b7f1acde7c8afd9ae5
Tony W <bobacrypto2@gmail.com>
2023-01-28 21:57:01 -0800

    Fibonacci - dynamic programming bottom-up

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function factorial(n) {
-    return n == 1 ? n : n * factorial(n - 1);
+function fib(n) {
+    let fibArr = [1, 1];
+    let sum = 0;
+    for (i = 0; i < n - 2; i++) {
+        sum = fibArr[i] + fibArr[i + 1];
+        fibArr.push(sum);
+    }
+    return sum;
 }
+
+// function fib(n) {
+//     let a = 1;
+//     let b = 1;
+//     for (let i = 3; i <= n; i++) {
+//         let c = a + b;
+//         a = b;
+//         b = c;
+//     }
+
+//     return b;
+// }

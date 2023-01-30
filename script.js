4477443837dc8b67e9a0d87fd8b2f67ffeea409c
Tony W <bobacrypto2@gmail.com>
2023-01-30 14:17:31 -0800

    recrusive power function

--- script.js
+++ script.js
@@ -1,36 +1,7 @@
-let list = {
-    value: 1,
-    next: {
-      value: 2,
-      next: {
-        value: 3,
-        next: {
-          value: 4,
-          next: null
-        }
-      }
-    }
-  };
-
-function printList(list) {
-    let arr = [];
-    let tmp = list;
-
-    while (tmp) {
-        arr.push(tmp.value);
-        tmp = tmp["next"];
-    }
-
-    for (let i = arr.length -1; i >= 0; i--) {
-        console.log(arr[i]);
-    }
-}
-
-function printList(list) {
-
-    if (list.next) {
-        printList(list.next);
-    }
-
-    console.log(list);
+function power(b, e) {
+  if (e == 0) {
+    return 1;
+  } else {
+    return b * power(b, e - 1);
+  }
 }

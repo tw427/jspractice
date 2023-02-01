92e2d61e79fadbd14346e94722be09106a5cd546
Tony W <bobacrypto2@gmail.com>
2023-01-31 22:21:37 -0800

    Fib function in a regular for loop

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function productOfArray(arr) {
-  if(arr.length == 0) {
-    return 1;
-  }
+function fibs(n) {
+    let array = [];
+    let nextFib;
 
-  return arr.shift() * productOfArray(arr);
+    for (i = 0; i < n; i++) {
 
+        if (array.length < 2) {
+            array.push(i);
+        } else {
+            nextFib = array[i - 1] + array[i - 2];
+            array.push(nextFib);
+        }
+    }
+
+    console.log(array);
 }
